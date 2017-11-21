3362a15e1cc278d6e5b2f20b9b276d45d38bba73
sherif fayez <sherifbaraya@gmail.com>
2017-11-21 14:51:15 -0500

    secound commit

--- module.js
+++ module.js
@@ -2,6 +2,18 @@
 
 
 	angular
-	  .module("storyApp", []);
+	  .module("storyApp", ["ngRoute"]);
+	  .config(function($routeProvider){
+
+	  	$routeProvider
+	  	.when("/form",{
+	  		template:`<form-component></form-component>`
+	  	})
+	  
+     .when("/story",{
+         template:`<story-component></form-component>`
+     })
+
+	  })
 
 })();
